9361e66144960fb8c1c6dc9ba83e9087a99a1257
eminarif <coderwax@gmail.com>
2022-12-07 15:31:32 +0300

    Updated routes\api

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
-Route::apiResource('/products', 'ProductController');
+Route::apiResource('/products', \App\Http\Controllers\ProductController::class);
 
 Route::group(['prefix' => 'products'], function () {
-	Route::apiResource('/{product}/reviews', 'ReviewController');
+	Route::apiResource('/{product}/reviews', \App\Http\Controllers\ReviewController::class);
 });
