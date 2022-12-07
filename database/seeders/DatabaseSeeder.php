c8565495d52ce7fee55965ef4f74b23cc917b981
eminarif <coderwax@gmail.com>
2022-12-07 13:08:35 +0300

    Updated DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+		\App\Models\Product::factory(50)->create();
+		\App\Models\Review::factory(300)->create();
     }
 }
