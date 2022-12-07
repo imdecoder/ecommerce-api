0ed9f07d4df887c0d8ac7db29f869a712f107469
eminarif <coderwax@gmail.com>
2022-12-07 16:23:12 +0300

    Updated ProductCollection

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 
@@ -16,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::paginate(20));
     }
 
     /**
--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,16 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+			'name' => $this->name,
+			'totalPrice' => number_format(($this->price - $this->discount), 2, ',', '.'),
+			'rating' => $this->reviews->count() > 0
+				? round($this->reviews->sum('star') / $this->reviews->count(), 2)
+				: 'No rating yet.',
+			'discount' => number_format($this->discount, 2, ',', '.'),
+			'href' => [
+				'link' => route('products.show', $this->id)
+			]
+		];
     }
 }
